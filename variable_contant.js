cbc68a999a324fbe903944b993b00039cd9d3b7e
souksavath-VLS <79986716+souksavath-VLS@users.noreply.github.com>
2022-01-08 15:38:07 +0700

    modify

--- variable_contant.js
+++ variable_contant.js
@@ -1,2 +1,20 @@
-let number = 56
+/**
+ * ປະກາດຕົວປ່ຽນທີ່ເຫມາະສົມໃຫ້ກັບຄ່າຕ່າງໆນີ້:
+ * - 56
+ * - 7.9
+ * - "Hello, Nodejs"
+ * - Pi = 22/7
+ * - n = 0
+ * - n < 3
+ */
+
+
+let number1 = 56
+const number2 = 7.9
+let x = '"Hello, Nodejs'
+const pi = 22/7
+const n = 0
+const n = n < 3
+
+
 
