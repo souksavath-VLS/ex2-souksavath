8b26d33abb15973988be2577bc61acc0a7c293fb
souksavath-VLS <79986716+souksavath-VLS@users.noreply.github.com>
2022-01-08 13:42:24 +0700

    modify

---
+++ fc_sequence.js
@@ -0,0 +1,67 @@
+/**
+ * ຈົ່ງແກ້ໄຂ function workOne, workTwo, workThree ໃຫ້ສະແດງຜົນໄດ້ຮັບທີ່ກຳນົດ
+ * ເມື່ອ run execute().
+ */
+
+
+ function workOne() {
+    /**
+     * coding
+     */
+    setTimeout(function () {
+      // edit
+      /**
+       * coding
+       */
+    }, 5000);
+    /**
+     * coding
+     */
+  }
+  
+  
+  function workTwo() {
+    /**
+     * coding
+     */
+    setTimeout(function () {
+      // edit
+      /**
+       * coding
+       */
+    }, 100);
+    /**
+     * coding
+     */
+  }
+  
+  
+  function workThree() {
+    /**
+     * coding
+     */
+    setTimeout(function () {
+      /**
+       * coding
+       */
+    }, 1000);
+    /**
+     * coding
+     */
+  }
+  
+  
+  let execute = async () => {
+    /**
+     * coding
+     */
+  };
+  
+  execute();
+  
+  /**
+   *  ==================================== Display ========================================
+   *  work 1
+   *  work 2
+   *  work 3
+   */
